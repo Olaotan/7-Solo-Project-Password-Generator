3b03f028842c8959fc564afaa7c2c0117fdd53b1
Olaotan <oeoladitan@hotmail.com>
2023-09-07 18:33:20 +0100

    files replaced

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordBtn = document.getElementById("password-btn")
 let passwordOne = document.getElementById("password-one")
 let passwordTwo = document.getElementById("password-two")
- 
+let passwordLength = document.getElementById("password-length")
+let passwordBtn = document.getElementById("password-btn")
+
 function getRandomNo(){
     let randomNo = Math.floor(Math.random()*characters.length)
     return randomNo
 }
-       
 
 function generatePassword(){
     passwordOne.textContent = ""
@@ -17,13 +17,11 @@ function generatePassword(){
     let password1 = ""
     let password2 = ""
     
-    for (let i = 0; i < 15; i++){
-        
+    for (let i = 0; i < passwordLength.value; i++){ 
     password1 += characters[getRandomNo()]
     password2 += characters[getRandomNo()]
     }
     
     passwordOne.textContent = password1
     passwordTwo.textContent = password2
-    
 }
